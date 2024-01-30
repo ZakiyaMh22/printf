7de31a7e778971f19406ec34e5967a8515d5a5f8
mohamedlayli <laylimohmd@gmail.com>
2024-01-30 01:27:01 -0800

    main.h updat

--- main.h
+++ main.h
@@ -5,7 +5,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_str(char *str);
 void _test(char c, va_list arg, int *ptr);
-
+void print_int(int num, int *length);
 
 
 #endif
